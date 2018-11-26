2d3924bf6fd10254fe147900f4f404ae32c2777a
jonnyboy6 <jonathan.narita@rocklinusd.org>
2018-11-26 15:53:09 -0800

    Updated Camera
    Changed from hsl to hsv in order to make it more accurate

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,22 @@
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
+import org.opencv.core.KeyPoint;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfKeyPoint;
+import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.features2d.FeatureDetector;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.core.Point;
 
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
@@ -23,8 +27,6 @@ import edu.wpi.cscore.VideoMode;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.tables.ITable;
 
-import com.pi4j.io.serial.*;
-
 public class Main {
   public static PrintWriter writer;
   static UsbCamera camera;
@@ -33,6 +35,9 @@ public class Main {
     writer.println(string);
     writer.close();
   }
+
+  static double resizeImageWidth = 320.0;
+  static double resizeImageHeight = 240.0;
 
   public static void main(String[] args) {
     int streamPort = 1185;
@@ -64,8 +69,9 @@ public class Main {
     NetworkTable.initialize();
     NetworkTable.getTable("CameraPublisher").getSubTable("T. J. Eckleburg").putStringArray("streams",
         new String[] { "mjpeg:http://" + PI_ADDRESS + ":" + PORT + "/stream.mjpg" });
-
-    camera.setResolution(640, 360);
+    int resWidth = 640;
+    int resHeight = 360;
+    camera.setResolution(resWidth, resHeight);
     camera.setFPS(7);
 
     CvSink imageSink = new CvSink("CV Image Grabber");
@@ -79,13 +85,14 @@ public class Main {
     MjpegServer cvStream = new MjpegServer("CV Image Stream", 1186);
     cvStream.setSource(imageSource);
 
+    MatOfKeyPoint blobsMat = new MatOfKeyPoint();
     Mat image = new Mat();
-    double[] hslThresholdHue = { 0.0, 50.98976109215018 };
-    double[] hslThresholdSaturation = { 91.72661870503596, 255.0 };
-    double[] hslThresholdLuminance = { 80.26079136690647, 255.0 };
+    double[] hsvThresholdHue = { 24.280575539568343, 61.740614334471 };
+    double[] hsvThresholdSaturation = { 50.44964028776978, 120.10238907849829 };
+    double[] hsvThresholdValue = { 123.83093525179855, 255.0 };
     Mat resizedImage = new Mat();
     Mat blurredImage = new Mat();
-    Mat HSL_Threshold = new Mat();
+    Mat HSV_Threshold = new Mat();
     Mat erode = new Mat();
     Mat dilate = new Mat();
     Mat empty = new Mat();
@@ -94,18 +101,16 @@ public class Main {
     org.opencv.core.Point anchor = new org.opencv.core.Point(-1, -1);
 
     // resize variables
-    double resizeImageWidth = 320.0;
-    double resizeImageHeight = 240.0;
+
     int resizeImageInterpolation = Imgproc.INTER_CUBIC;
 
     // blur variables
-    double cvMedianblurKsize = 13.0;
+    double cvMedianblurKsize = 7.0;
 
     // Blob Variables
     double findBlobsMinArea = 1.0;
     double[] findBlobsCircularity = { 0.0, 1.0 };
     boolean findBlobsDarkBlobs = false;
-    MatOfKeyPoint blobsMat = new MatOfKeyPoint();
 
     while (true) {
       long frameTime = imageSink.grabFrame(image);
@@ -118,20 +123,52 @@ public class Main {
       cvMedianblur(resizedImage, cvMedianblurKsize, blurredImage);
 
       // Step HSL_Threshold:
-      hslThreshold(blurredImage, hslThresholdHue, hslThresholdSaturation, hslThresholdLuminance, HSL_Threshold);
+      hsvThreshold(blurredImage, hsvThresholdHue, hsvThresholdSaturation, hsvThresholdValue, HSV_Threshold);
 
       // Step CV_erode:
-      cvErode(HSL_Threshold, empty, anchor, erodeAndDilateIterations, Core.BORDER_CONSTANT, Bordervalue, erode);
+      cvErode(HSV_Threshold, empty, anchor, erodeAndDilateIterations, Core.BORDER_CONSTANT, Bordervalue, erode);
 
       // Step CV_dilate:
       cvDilate(erode, empty, anchor, erodeAndDilateIterations, Core.BORDER_CONSTANT, Bordervalue, dilate);
 
       // Step Find_Blobs:
-      findBlobs(dilate, findBlobsMinArea, findBlobsCircularity, findBlobsDarkBlobs, blobsMat);
+      findBlobs(HSV_Threshold, findBlobsMinArea, findBlobsCircularity, findBlobsDarkBlobs, blobsMat);
 
-      imageSource.putFrame(HSL_Threshold);
+      imageSource.putFrame(HSV_Threshold);
+      // NetworkTable.getTable("SmartDashboard").putString("Yellow Ball",
+      // center(blobsMat));
+
+      NetworkTable.getTable("SmartDashboard").putString("Yellow Ball", "" + blob(blobsMat)[0]);
     }
 
+  }
+
+  public static String center(MatOfKeyPoint blobs) {
+    double deadzone = 50.0;// This is the width of the area in the center that counts as "centered"
+    if (blob(blobs)[0] != -1) {
+      if (blob(blobs)[0] < (resizeImageWidth - deadzone) / 2) {
+        return "left";
+      } else if (blob(blobs)[0] > (resizeImageWidth - deadzone) / 2) {
+        return "right";
+      } else {
+        return "centered";
+      }
+    }
+    return "no blob";
+  }
+
+  public static int[] blob(MatOfKeyPoint blobs) {
+    List<KeyPoint> list = blobs.toList();
+    int largest = 0;// index of largest blob
+    if (!list.isEmpty()) {
+      for (int i = 0; i < list.size(); i++) {
+        if (list.get(i).size > list.get(largest).size) {
+          largest = i;
+        }
+      }
+      return new int[] { (int) list.get(largest).pt.x, (int) list.get(largest).pt.y };
+    }
+    return new int[] { -1, -1 };// No point found
   }
 
   /**
@@ -167,9 +204,9 @@ public class Main {
    * @param lum    The min and max luminance
    * @param output The image in which to store the output.
    */
-  private static void hslThreshold(Mat input, double[] hue, double[] sat, double[] lum, Mat out) {
-    Imgproc.cvtColor(input, out, Imgproc.COLOR_BGR2HLS);
-    Core.inRange(out, new Scalar(hue[0], lum[0], sat[0]), new Scalar(hue[1], lum[1], sat[1]), out);
+  private static void hsvThreshold(Mat input, double[] hue, double[] sat, double[] val, Mat out) {
+    Imgproc.cvtColor(input, out, Imgproc.COLOR_BGR2HSV);
+    Core.inRange(out, new Scalar(hue[0], sat[0], val[0]), new Scalar(hue[1], sat[1], val[1]), out);
   }
 
   /**
@@ -243,7 +280,53 @@ public class Main {
    */
   private static void findBlobs(Mat input, double minArea, double[] circularity, Boolean darkBlobs,
       MatOfKeyPoint blobList) {
+
     FeatureDetector blobDet = FeatureDetector.create(FeatureDetector.SIMPLEBLOB);
+    try {
+      File tempFile = File.createTempFile("config", ".xml");
+
+      StringBuilder config = new StringBuilder();
+
+      config.append("<?xml version=\"1.0\"?>\n");
+      config.append("<opencv_storage>\n");
+      config.append("<thresholdStep>10.</thresholdStep>\n");
+      config.append("<minThreshold>50.</minThreshold>\n");
+      config.append("<maxThreshold>220.</maxThreshold>\n");
+      config.append("<minRepeatability>2</minRepeatability>\n");
+      config.append("<minDistBetweenBlobs>10.</minDistBetweenBlobs>\n");
+      config.append("<filterByColor>1</filterByColor>\n");
+      config.append("<blobColor>");
+      config.append((darkBlobs ? 0 : 255));
+      config.append("</blobColor>\n");
+      config.append("<filterByArea>1</filterByArea>\n");
+      config.append("<minArea>");
+      config.append(minArea);
+      config.append("</minArea>\n");
+      config.append("<maxArea>");
+      config.append(Integer.MAX_VALUE);
+      config.append("</maxArea>\n");
+      config.append("<filterByCircularity>1</filterByCircularity>\n");
+      config.append("<minCircularity>");
+      config.append(circularity[0]);
+      config.append("</minCircularity>\n");
+      config.append("<maxCircularity>");
+      config.append(circularity[1]);
+      config.append("</maxCircularity>\n");
+      config.append("<filterByInertia>1</filterByInertia>\n");
+      config.append("<minInertiaRatio>0.1</minInertiaRatio>\n");
+      config.append("<maxInertiaRatio>" + Integer.MAX_VALUE + "</maxInertiaRatio>\n");
+      config.append("<filterByConvexity>1</filterByConvexity>\n");
+      config.append("<minConvexity>0.95</minConvexity>\n");
+      config.append("<maxConvexity>" + Integer.MAX_VALUE + "</maxConvexity>\n");
+      config.append("</opencv_storage>\n");
+      FileWriter writer;
+      writer = new FileWriter(tempFile, false);
+      writer.write(config.toString());
+      writer.close();
+      blobDet.read(tempFile.getPath());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
     blobDet.detect(input, blobList);
   }
 
